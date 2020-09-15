a2fd4d7c18c741ec48c553b2f8f8a8387a1a2702
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_mysql/ubuntu/__init__.py
+++ offregister_mysql/ubuntu/__init__.py
@@ -6,18 +6,25 @@ from offregister_fab_utils.ubuntu.systemd import restart_systemd
 
 
 def install0(**kwargs):
-    installed = lambda: run('mysqld --version | while read _ _ ver _; do echo "$ver"; done', quiet=True)
+    installed = lambda: run(
+        'mysqld --version | while read _ _ ver _; do echo "$ver"; done', quiet=True
+    )
 
-    if sudo('dpkg -s mysql-server', quiet=True, warn_only=True).failed:
-        with shell_env(DEBIAN_FRONTEND='noninteractive'):
+    if sudo("dpkg -s mysql-server", quiet=True, warn_only=True).failed:
+        with shell_env(DEBIAN_FRONTEND="noninteractive"):
             # TODO: Better password handling; I think this can get leaked, even with `quiet=True`?
-            sudo('''
+            sudo(
+                """
             debconf-set-selections <<< 'mysql-server mysql-server/root_password password {password}';
             debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password {password}';
-            '''.format(password=kwargs['MYSQL_PASSWORD']), quiet=True)
-            apt_depends('mysql-server', 'mysql-client', 'libmysqlclient-dev')
-            sudo('systemctl unmask mysql')
-            restart_systemd('mysql')
-            return 'MySQL {} installed'.format(installed())
+            """.format(
+                    password=kwargs["MYSQL_PASSWORD"]
+                ),
+                quiet=True,
+            )
+            apt_depends("mysql-server", "mysql-client", "libmysqlclient-dev")
+            sudo("systemctl unmask mysql")
+            restart_systemd("mysql")
+            return "MySQL {} installed".format(installed())
 
-    return '[Already] MySQL {} installed'.format(installed())
+    return "[Already] MySQL {} installed".format(installed())
